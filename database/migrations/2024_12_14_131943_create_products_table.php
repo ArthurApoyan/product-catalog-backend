64cd69fdce22dfa42bcf3e75453185f295e7fd61
ArthurApoyan <arthur.apoyan.96@gmail.com>
2024-12-17 00:26:40 +0400

    Added length limits for string fields for deployment

--- database/migrations/2024_12_14_131943_create_products_table.php
+++ database/migrations/2024_12_14_131943_create_products_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description');
+            $table->string('name', 191);
+            $table->string('description', 191);
             $table->decimal('price', 8, 2);
-            $table->string('image')->nullable();
+            $table->string('image', 191)->nullable();
             $table->unsignedBigInteger('category_id');
             $table->timestamps();
 
